ebdae334ffc21034558abac6094995664e452c83
Z-Akdim <zakaria.akdim37@gmail.com>
2024-05-20 21:34:25 +0100

    feat: install tailwind package with config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Sidebar from "./components/SideBar";
+
 const App = () => {
-  return <div>music room</div>;
+  return (
+    <div className="h-screen bg-black">
+      <div className="flex h-[90%]">
+        <Sidebar />
+      </div>
+    </div>
+  );
 };
 
 export default App;
