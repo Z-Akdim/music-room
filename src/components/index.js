b6ba520bcebcff2ccc5ef3ec883d24a54a85de39
Z-Akdim <zakaria.akdim37@gmail.com>
2024-05-21 23:41:54 +0100

    feat: create discover page and some components

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as SongCard } from "./SongCard";
+export { default as TopPlay } from "./TopPlay";
+export { default as SideBar } from "./SideBar";
+export { default as SearchBar } from "./SearchBar";
