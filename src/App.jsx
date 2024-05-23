3bbc34cda94cd9bae56390a54b0503529eff03b5
Z-Akdim <zakaria.akdim37@gmail.com>
2024-05-23 22:00:26 +0100

    add some assets and creat sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-import { SideBar, TopPlay, SearchBar } from "./components";
-import { Discover } from "./pages";
+import { SideBar, Player } from "./components";
 
 const App = () => {
   return (
-    <div className="relative flex">
-      <SideBar />
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
-        <SearchBar />
-
-        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
-          <div className="flex-1 h-fit pb-40">
-            <Routes>
-              <Route path="/" element={<Discover />} />
-            </Routes>
-          </div>
-          <div className="xl:sticky relative top-0 h-fit">
-            <TopPlay />
-          </div>
-        </div>
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <SideBar />
       </div>
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as SongCard } from "./SongCard";
-export { default as TopPlay } from "./TopPlay";
+export { default as Player } from "./Player";
 export { default as SideBar } from "./SideBar";
 export { default as SearchBar } from "./SearchBar";
