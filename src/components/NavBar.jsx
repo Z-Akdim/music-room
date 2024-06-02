4c01c41173a6cdd214b1f3a8781fdbcff257a265
Z-Akdim <zakaria.akdim37@gmail.com>
2024-06-02 16:40:12 +0100

    feat: add display album ui

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,22 @@
 import { assets } from "../assets/assets";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isAlbum = location.pathname.includes("album");
   return (
     <>
       <div className="w-full flex justify-between items-center font-semibold">
         <div className="flex items-center gap-2">
           <img
+            onClick={() => isAlbum && navigate(-1)}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             src={assets.arrow_left}
             alt=""
           />
           <img
+            onClick={() => navigate(1)}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             src={assets.arrow_right}
             alt=""
